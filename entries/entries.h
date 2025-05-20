d8e7bdf2113967570c5fc2d42209bed5c165e7db
Mathieu BARLEON <mathieu.barleon@epitech.eu>
2025-05-20 09:33:09 +0200

    Feat: added entrypoints

---
+++ entries/entries.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** coding-style-war-crimes
+** File description:
+** c_extend_log_init_entry
+*/
+
+#ifndef C_EXTEND_ENTRIES_H_
+    #define C_EXTEND_ENTRIES_H_
+    #include "../include/macro.h"
+
+typedef enum {
+    C_EXTEND_MIN_ENTRY = 101,
+    C_EXTEND_INIT_LOG_ENTRY
+} c_extend_constructor_priotiries_t;
+
+#endif /* !C_EXTEND_ENTRIES_H_ */
---
+++ entries/init_log_entry.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2025
+** coding-style-war-crimes
+** File description:
+** zap_srv_init_log_entry
+*/
+
+#include "entries.h"
+#include "../include/logger.h"
+
+CTOR(C_EXTEND_INIT_LOG_ENTRY)
+void c_extend_init_log_entry(void)
+{
+    C_EXTEND_INIT_LOG;
+}
